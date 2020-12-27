ee74beb78687e7a166dcba595107325057d96e29
ri003074 <ri003074@gmail.com>
2020-12-27 18:36:29 +0900

    add axios, eventlister

--- frontend/pages/add.tsx
+++ frontend/pages/add.tsx
@@ -1,33 +1,36 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
-import {Form, Col, Button} from 'react-bootstrap'
+import { Form, Col, Button } from 'react-bootstrap'
+import axios from 'axios'
 
 export default function Add() {
-    function clickTest(e){
+    function clickTest(e) {
         console.log("hehe")
-        console.log(e)
+        console.log(e.target)
+        e.preventDefault()
     }
     return (
         <Layout>
             <Head>
                 <title>Add</title>
             </Head>
-            
-            <Form.Group>
-                <Col sm="4" className="mb-2">
-                    <Form.Control size="sm" type="text" placeholder="English Phrase" />
-                </Col>
-                <Col sm="4" className="mb-2">
-                    <Form.Control size="sm" type="text" placeholder="Japanese Phrase" />
-                </Col>
-                <Col sm="4">
-                    <Form.Control size="sm" type="text" placeholder="English Word" />
-                </Col>
-            </Form.Group>
-            <Button className="ml-3" variant="primary" type="submit" onClick={clickTest}>
-                Submit
-            </Button>
 
+            <form onSubmit={clickTest}>
+                <Form.Group>
+                    <Col sm="4" className="mb-2">
+                        <Form.Control size="sm" type="text" name="english_phrase" value="a" placeholder="English Phrase" />
+                    </Col>
+                    <Col sm="4" className="mb-2">
+                        <Form.Control size="sm" type="text" name="japanese_phrase" placeholder="Japanese Phrase" />
+                    </Col>
+                    <Col sm="4">
+                        <Form.Control size="sm" type="text" name="english_word" placeholder="English Word" />
+                    </Col>
+                </Form.Group>
+                <Button className="ml-3" variant="primary" type="submit" >
+                    Submit
+            </Button>
+            </form>
         </Layout>
     )
 }
---
+++ frontend/pages/quiz.tsx
@@ -0,0 +1,25 @@
+import Layout from '../components/layout'
+import Head from 'next/head'
+import React from 'react'
+
+export default function Quiz() {
+
+    function keydown(e) {
+        console.log(e.key)
+    }
+
+    React.useEffect(() => {
+        window.addEventListener('keydown', keydown)
+    }, [])
+
+    return (
+        <Layout>
+            <Head>
+                <title>Quiz</title>
+            </Head>
+            <div>
+                Quiz
+            </div>
+        </Layout>
+    )
+}
