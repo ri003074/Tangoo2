96e2884525742f06c096836f0ef14f980e5a02b0
ri003074 <ri003074@gmail.com>
2020-12-31 14:13:39 +0900

    small fix

--- frontend/components/DisplayComponent.tsx
+++ frontend/components/DisplayComponent.tsx
@@ -7,12 +7,12 @@ export default function Display({ contents }) {
             <thead>
                 {contents.map((content, index) =>
                     <tr key={content.id}>
-                        <td style={{ width: '10%', textAlign: 'center' }}>{index}</td>
+                        <td style={{ width: '10%', textAlign: 'center' }}>{('000' + index).slice(-3)}</td>
                         <td style={{ width: '30%' }}>{content.phrase_en}</td>
                         <td style={{ width: '25%' }}>{content.phrase_ja}</td>
                         <td style={{ width: '15%' }}>{content.word_en}</td>
-                        <td style={{ width: '10%' }}>{((content.c_counter / content.s_counter) * 100).toFixed(0)} %</td>
-                        <td style={{ width: '10%' }}>[x]</td>
+                        <td style={{ width: '10%', textAlign: 'center' }}>{((content.c_counter / content.s_counter) * 100).toFixed(1)}%</td>
+                        <td style={{ width: '10%', textAlign: 'center' }}>[x]</td>
                     </tr>
                 )}
             </thead>
--- frontend/components/layout.tsx
+++ frontend/components/layout.tsx
@@ -3,18 +3,17 @@ import { Navbar } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Layout({ children }) {
+
     return (
         <div>
-            <div>
-                <Navbar>
-                    <Navbar.Brand>
-                        <Link href="/">Tangoo</Link>
-                    </Navbar.Brand>
-                    <Link href="/">Home</Link>
-                    <Link href="/add">Add</Link>
-                    <Link href="/quiz">Quiz</Link>
-                </Navbar>
-            </div>
+            <Navbar>
+                <Navbar.Brand>
+                    <Link href="/" >Tangoo</Link>
+                </Navbar.Brand>
+                <Link href="/">Home</Link>
+                <Link href="/add">Add</Link>
+                <Link href="/quiz">Quiz</Link>
+            </Navbar>
             {children}
         </div>
     )
